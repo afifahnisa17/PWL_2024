f63d966024384396772cb6435a24ca1e6c2a00b9
afifahnisa17 <afifahnisa041@gmail.com>
2025-02-19 09:41:53 +0700

    route parameters

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,23 @@ Route::get('/', function(){
 Route::get('/about', function(){
     return 'NIM: 2341720250, Nama: Afifah Khoirunnisa';
 });
+
+Route::get('/user/{name}', function($name){
+    return 'Nama saya '. $name;
+});
+
+Route::get('/user/{afifah}', function($afifah){
+    return 'Nama saya '. $afifah;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Post ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function($postId){
+    return 'Halaman Artikel dengan ID: '. $postId;
+});
+
+// Route::get('/user/{name?', function($name='John'){
+//     return 'Nama saya'. $name;
+// });
