b611ed3ff12027f665fcdf734cd1aa43b37be322
afifahnisa17 <afifahnisa041@gmail.com>
2025-02-19 09:09:19 +0700

    Basic Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('World', function(){
+    return 'World';
+});
+
+Route::get('/', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return 'NIM: 2341720250, Nama: Afifah Khoirunnisa';
 });
