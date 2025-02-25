f9e88bdbdf5c67e8abd2f6a919a691f571765aa8
afifahnisa17 <afifahnisa041@gmail.com>
2025-02-25 21:16:14 +0700

    add file hello.blade.php, update route

--- routes/web.php
+++ routes/web.php
@@ -119,6 +119,9 @@ Route::resource('photos', PhotoController::class) ->only([
 Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
 ]);
 
+Route::get('/greeting', function(){
+    return view('hello', ['name' => 'Afifah']);
+});
 
 
 
